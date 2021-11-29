0b32615b53e11b7dc8eb5df566c5a8a29520e630
ky chen <1260198106@qq.com>
2021-11-30 04:50:42 +0800

    实现一个新的页面，并实现两个页面之间的跳转

--- game/views.py
+++ game/views.py
@@ -4,6 +4,16 @@ from django.http import HttpResponse
 
 def index(request):
     line1 = '<h1 style="text-align: center">术士之战</h1>'
+    line4 = '<a href="/play/">进入游戏界面</a>'
     line3 = '<hr>'
-    line2 = '<img src="https://img1.baidu.com/it/u=4035339947,1492935398&fm=26&fmt=auto" width=2000>'
-    return HttpResponse(line1 + line3 + line2)
+    line2 = '<img src="https://img1.baidu.com/it/u=4035339947,1492935398&fm=26&fmt=auto" width=1000>'
+    return HttpResponse(line1 + line4 + line3 + line2)
+
+def play(request):
+    line1 = '<h1 style="text-align: center">游戏界面</h1>'
+    line4 = '<a href="/">返回上一页面</a>'
+    line3 = '<hr>'
+    line2 = '<img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fnimg.ws.126.net%2F%3Furl%3Dhttp%3A%2F%2Fdingyue.ws.126.net%2F2021%2F0511%2Fc25377afj00qsx6kz002cd000h000msp.jpg%26thumbnail%3D650x2147483647%26quality%3D80%26type%3Djpg&refer=http%3A%2F%2Fnimg.ws.126.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1640810824&t=25a43dd2868abade40fc5405f704b8c3" width=1000>'
+    return HttpResponse(line1 + line4 + line3 + line2)
+
+
