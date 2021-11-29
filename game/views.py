6ee9694dc0d396ea6d48950fc3061cbc9243bf1e
ky chen <1260198106@qq.com>
2021-11-30 04:27:26 +0800

    实现了主路径和第一个页面，强者的强

--- game/views.py
+++ game/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    line1 = '<h1 style="text-align: center">术士之战</h1>'
+    line3 = '<hr>'
+    line2 = '<img src="https://img1.baidu.com/it/u=4035339947,1492935398&fm=26&fmt=auto" width=2000>'
+    return HttpResponse(line1 + line3 + line2)
