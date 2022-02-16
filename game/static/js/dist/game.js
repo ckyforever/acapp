e81e16baf741a1eb8d206c5e15b459c32a29c985
ky chen <1260198106@qq.com>
2022-02-16 15:35:03 +0800

    完成 acapp 的对接

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -525,16 +525,7 @@ class Partcle extends AcGameObject {
         this.root = root;
         this.$playground = $(`<div class="ac-game-playground"></div>`);
 
-        //this.hide();
-        this.root.$ac_game.append(this.$playground);
-        this.width = this.$playground.width();
-        this.height = this.$playground.height();
-        this.game_map = new GameMap(this);
-        this.players = [];
-        this.players.push(new Player(this,this.width / 2,this.height / 2,this.height * 0.05,"white",this.height * 0.2,true));
-        for(let i = 0;i < 5;i ++) {
-            this.players.push(new AiPlayer(this,this.width / 2,this.height / 2,this.height * 0.05,this.get_random_color(),this.height * 0.2));
-        }
+        this.hide();
 
         this.start();
     }
@@ -549,6 +540,15 @@ class Partcle extends AcGameObject {
 
     show() {  // 打开playground界面
         this.$playground.show();
+        this.root.$ac_game.append(this.$playground);
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        this.game_map = new GameMap(this);
+        this.players = [];
+        this.players.push(new Player(this,this.width / 2,this.height / 2,this.height * 0.05,"white",this.height * 0.2,true));
+        for(let i = 0;i < 5;i ++) {
+            this.players.push(new AiPlayer(this,this.width / 2,this.height / 2,this.height * 0.05,this.get_random_color(),this.height * 0.2));
+        }
     }
 
     hide() {  // 关闭playground界面
--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -3,16 +3,7 @@ class AcGamePlayground {
         this.root = root;
         this.$playground = $(`<div class="ac-game-playground"></div>`);
 
-        //this.hide();
-        this.root.$ac_game.append(this.$playground);
-        this.width = this.$playground.width();
-        this.height = this.$playground.height();
-        this.game_map = new GameMap(this);
-        this.players = [];
-        this.players.push(new Player(this,this.width / 2,this.height / 2,this.height * 0.05,"white",this.height * 0.2,true));
-        for(let i = 0;i < 5;i ++) {
-            this.players.push(new AiPlayer(this,this.width / 2,this.height / 2,this.height * 0.05,this.get_random_color(),this.height * 0.2));
-        }
+        this.hide();
 
         this.start();
     }
@@ -27,6 +18,15 @@ class AcGamePlayground {
 
     show() {  // 打开playground界面
         this.$playground.show();
+        this.root.$ac_game.append(this.$playground);
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        this.game_map = new GameMap(this);
+        this.players = [];
+        this.players.push(new Player(this,this.width / 2,this.height / 2,this.height * 0.05,"white",this.height * 0.2,true));
+        for(let i = 0;i < 5;i ++) {
+            this.players.push(new AiPlayer(this,this.width / 2,this.height / 2,this.height * 0.05,this.get_random_color(),this.height * 0.2));
+        }
     }
 
     hide() {  // 关闭playground界面
